15daeedc5d757e1d5aadda6170f82c7f8dc02452
Uros <umitrovic22@gmail.com>
2021-12-02 21:12:55 +0100

    Izmene na business sloju

---
+++ BusinessLayer/DataLayer/StudentRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Models;
+
+namespace DataLayer
+{
+    public class StudentRepository
+    {
+        public List<Student> GetAllStudents()
+        {
+            List<Student> res = new List<Student>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Students";
+                sqlConnection.Open();
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while(sqlDataReader.Read())
+                {
+                    Student s = new Student();
+                    s.Id = sqlDataReader.GetInt32(0);
+                    s.Name = sqlDataReader.GetString(1);
+                    s.IndexNumber = sqlDataReader.GetString(2);
+                    s.AverageMark = sqlDataReader.GetDecimal(3);
+
+                    res.Add(s);
+                }
+                
+            }
+      
+            return res;
+        }
+        public int InsertStudent(Student s)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = string.Format(format: "INSERT INTO Students VALUES('{0}','{1}',{2})",s.Name,s.IndexNumber,s.AverageMark);
+                
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+     }
+               
+ }
+    
+
+        
+
+   
+    
